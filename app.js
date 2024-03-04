acca66aad141002854fbee574029a8fc3ad87902
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-03-04 18:55:59 -0300

    Update app.js
    Concatenação e tomada de decisão

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert("Boas vindas ao jogo!");
-let numeroSecreto = 5;
+let numeroSecreto = 4;
+console.log(numeroSecreto);
 let chute = prompt("Escolha um numero entre 1 e 10");
 
+// se o chute for igual ao número secerto
 if(chute == numeroSecreto){
-    console.log("Você acertou!");
+    alert(`Você acertou! O número secreto é ${numeroSecreto}`);
+} else {
+    alert("Você errou!");
 }
