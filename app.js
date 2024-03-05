6c53d96c9d338981b8f4ba64c7d901a38da49336
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-03-05 20:18:11 -0300

    Update app.js
    Adicionando laço de repetição e utilizando incremento.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 alert("Boas vindas ao jogo!");
 let numeroSecreto = 4;
 console.log(numeroSecreto);
-let chute = prompt("Escolha um numero entre 1 e 10");
+let chute;
+let tentativas = 1;
 
-// se o chute for igual ao número secerto
-if(chute == numeroSecreto){
-    alert(`Você acertou! O número secreto é ${numeroSecreto}`);
-} else {
-    alert("Você errou!");
+// enquanto o chute não for igual ao número secreto
+while(chute != numeroSecreto){
+
+    chute = prompt("Escolha um número entre 1 e 10");
+
+    // se o chute for igual ao número secerto
+    if(chute == numeroSecreto){
+        alert(`Você acertou! O número secreto é ${numeroSecreto} e você descobriu com ${tentativas}.`);
+    } else {
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}.`);
+        } else{
+            alert(`O número secreto é maior que ${chute}.`);
+        }
+        tentativas++;
+    }
 }
