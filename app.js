6ff3c91bb352b0e914d896bd23f7a9c4ef9dbe83
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-03-06 20:26:12 -0300

    Update app.js
    Adicionado a geração de números pseudo aleatórios e utilizando operador ternário para variação de número gramatical (singular ou plural) da palavra "tentativa".

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 alert("Boas vindas ao jogo!");
-let numeroSecreto = 4;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto o chute não for igual ao número secreto
 while(chute != numeroSecreto){
-
-    chute = prompt("Escolha um número entre 1 e 10");
-
-    // se o chute for igual ao número secerto
+    chute = prompt("Escolha um número entre 1 e 100");
+    // se o chute for igual ao número secreto
     if(chute == numeroSecreto){
-        alert(`Você acertou! O número secreto é ${numeroSecreto} e você descobriu com ${tentativas}.`);
+        break;
     } else {
         if(chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}.`);
         } else{
             alert(`O número secreto é maior que ${chute}.`);
         }
+        // tentativas = tentativas + 1
         tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você acertou! O número secreto é ${numeroSecreto} e você descobriu com ${tentativas} ${palavraTentativa}.`);
