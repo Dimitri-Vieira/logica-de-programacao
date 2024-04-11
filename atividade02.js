7be844d78cefb7cc478487a2aa6c73468e757142
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-04-11 20:36:35 -0300

    Arquivos da segunda atividade
    Adição dos arquivos da segunda lista de atividades sugeridas pelo curso.

---
+++ atividade02.js
@@ -0,0 +1,47 @@
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 01
+
+// let contador = 1;
+
+// // enquanto o contador for menor ou igual a 10 (Imprime no console)
+// while(contador <= 10){
+//     console.log(contador);
+//     contador++;
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 01
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 02
+
+// let contador = 10;
+
+// // enquanto o contador for maior ou igual a 0 (Imprime no console)
+// while(contador >= 0){
+//     console.log(contador);
+//     contador--;
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 02
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 03
+
+// let contador = prompt("Insira um número de partida para a contagem regressiva.");
+
+// // enquanto o contador for maior ou igual a 0 (Imprime no console)
+// while(contador >= 0){
+//     console.log(contador);
+//     contador--;
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 03
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 04
+
+// let numeroInserido = prompt("Insira um número para contagem progressiva.");
+// let numeroProgressor = 0;
+
+// // enquanto o número progressor for menor ou igual ao número inserido
+// while(numeroProgressor <= numeroInserido){
+//     console.log(numeroProgressor);
+//     numeroProgressor++;
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 04
