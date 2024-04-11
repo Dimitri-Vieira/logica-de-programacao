6c8525c173c189519de9993399c84e71665bb1a7
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-04-11 19:52:29 -0300

    Arquivos da primeira atividade
    Inclusão de todos os arquivos da Atividade 01 proposta durante o curso.

---
+++ atividade01.js
@@ -0,0 +1,62 @@
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 01
+
+// alert("Olá! Em que dia da semana estamos?");
+// let diaDaSemana = prompt('Digite o dia da semana, por favor:');
+// console.log(diaDaSemana);
+
+// // se o dia da semana for igual a "Sábado" ou "Domingo"
+// if(diaDaSemana == "Sábado" || diaDaSemana == "Domingo"){
+//     alert("Obrigado! Bom fim de semana!");
+// } 
+// // se não for
+// else{
+//         alert("Obrigado! Boa semana!");
+//     }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 01
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 02
+
+// alert("Deseja saber se o número é positivo ou negativo?");
+// let num = prompt("Digite o número:");
+// console.log(num);
+
+// // se número for maior que zero
+// if(num > 0){
+//     alert(`O número ${num} é positivo.`);
+// } else if(num == 0){
+//     alert(`O número ${num} é um número neutro, portanto, não é negativo e nem positivo.`);
+// } else{
+//     alert(`O número ${num} é negativo.`);
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 02
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 03
+
+// let pontuacao = prompt("Insira o valor da pontuação obtida no jogo.");
+
+// // se a pontuação for maior ou igual a 100 pontos
+// if(pontuacao >= 100){
+//     console.log("Parabéns, você venceu!");
+// }else{
+//     console.log("Tente novamente para ganhar.");
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 03
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 04
+
+// // iniciando com um valor
+// let saldoEmConta = 100;
+
+// alert(`Seu saldo em conta é de R$${saldoEmConta}.`);
+// // FINAL DO EXERCÍCIO DA QUESTÃO 04
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 05
+
+// let nomeUsuario = prompt("Insira seu nome.");
+
+// alert(`Boas vindas, ${nomeUsuario}`);
+// // FINAL DO EXERCÍCIO DA QUESTÃO 05
