4cc624d63d28ee5d4a094042478b6f23605b1f02
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-04-11 21:46:22 -0300

    Arquivos da terceira atividade
    Adição de arquivos da terceira lista de atividades proposta pelo curso.

---
+++ atividade03.js
@@ -0,0 +1,95 @@
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 01 a 04
+
+// console.log("Boas-vindas!");
+// let nome = "Dimitri";
+// console.log(`Olá, ${nome}!`);
+// alert(`Olá, ${nome}!`);
+
+// let linguagemPreferida = prompt("Qual a linguagem de programação que você mais gosta?");
+// console.log(linguagemPreferida);
+// // FINAL DO EXERCÍCIO DA QUESTÃO 01 a 04
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 05
+
+// let valor1 = 5;
+// let valor2 = 9;
+// let resultado = valor1 + valor2;
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+// // FINAL DO EXERCÍCIO DA QUESTÃO 05
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 06
+// let valor1 = 5;
+// let valor2 = 9;
+// let resultado = valor1 - valor2;
+// console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}.`);
+// // FINAL DO EXERCÍCIO DA QUESTÃO 06
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 07
+
+// let idade = prompt("Insira sua idade.");
+// // se a idade inserida for maior ou igual a 18
+// if(idade >= 18){
+//     console.log("O usuário é maior de idade.");
+// }else{
+//     console.log("O usuário é menor de idade.");
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 07
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 08
+
+// let numero = prompt("Insira um número para verificar se ele é positivo, negativo ou neutro.");
+
+// if(numero == 0){
+//     alert(`O número ${numero} é neutro.`);
+
+// }else if(numero > 0){
+//     alert(`O número ${numero} é positivo.`);
+// }else if(numero < 0){
+//     alert(`O número ${numero} é negativo.`);
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 08
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 09
+
+// let contador = 1;
+
+// // enquanto o contador for menor ou igual a 10
+// while(contador <= 10){
+//     console.log(contador);
+//     contador++;
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 09
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 10
+
+// let nota = prompt("Insira a o valor da nota obtida.");
+
+// // se a nota for maior ou igual a 7
+// if(nota >= 7){
+//     console.log("Aprovado");
+// }else{
+//     console.log("Reprovado");
+// }
+// // FINAL DO EXERCÍCIO DA QUESTÃO 10
+
+
+// // INÍCIO DO EXERCÍCIO DA QUESTÃO 11 a 13
+
+// // gerando número aleatório qualquer
+// let numeroQualquer = Math.random() * 10;
+// console.log(numeroQualquer);
+
+// // gerando número inteiro entre 1(inclusivo) e 10(inclusivo)
+// let numeroInteiroDez = parseInt(Math.random() * 10 + 1);
+// console.log(numeroInteiroDez);
+
+// // gerando número inteiro entre 1(inclusivo) e 1000(inclusivo)
+// let numeroInteiroMil = parseInt(Math.random() * 1000 + 1);
+// console.log(numeroInteiroMil);
+// // FINAL DO EXERCÍCIO DA QUESTÃO 11 a 13
